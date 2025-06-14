7b409b950a8f1206d66289ecd33678f916c4c9b6
Oki <biek.syaoki@gmail.com>
2025-06-14 20:53:47 +0700

    feat: Project init

---
+++ Chlorophyll/Chlorophyll/ChlorophyllApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChlorophyllApp.swift
+//  Chlorophyll
+//
+//  Created by Syaoki Biek on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChlorophyllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chlorophyll/Chlorophyll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chlorophyll
+//
+//  Created by Syaoki Biek on 14/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
