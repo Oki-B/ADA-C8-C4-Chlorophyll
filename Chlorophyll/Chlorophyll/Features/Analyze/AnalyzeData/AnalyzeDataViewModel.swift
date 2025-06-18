d41452960e6b713631b6f64c126992f447e585a0
Oki <biek.syaoki@gmail.com>
2025-06-18 17:05:47 +0700

    fix: replace model

--- Chlorophyll/Chlorophyll/Features/Analyze/AnalyzeData/AnalyzeDataViewModel.swift
+++ Chlorophyll/Chlorophyll/Features/Analyze/AnalyzeData/AnalyzeDataViewModel.swift
@@ -1,97 +1,97 @@
-////
-////  AnalyzeDataViewModel.swift
-////  Chlorophyll
-////
-////  Created by Syaoki Biek on 17/06/25.
-////
 //
-//import CoreML
-//import Foundation
+//  AnalyzeDataViewModel.swift
+//  Chlorophyll
 //
-//class AnalyzeDataViewModel: ObservableObject {
-//    @Published var result: String?
-//    @Published var error: Error?
-//    @Published var isAnalyzing = false
-//    @Published var nitrogenLevel: Double?
-//    @Published var phosphorusLevel: Double?
-//    @Published var potassiumLevel: Double?
+//  Created by Syaoki Biek on 17/06/25.
 //
-//    @Published var plantHealth: String?
-//    @Published var plantHealthProbability: [String: Double]?
-//
-//    func predictHealth(moist: Double? = nil, temp: Double? = nil, hum: Double? = nil, soilPH: Double? = nil) {
-//        do {
-//            let config = MLModelConfiguration()
-//            let model = try PlantStressClassifier(configuration: config)
-//
-//            var prediction: PlantStressClassifierOutput!
-//            
-//            if let moist = moist, let temp = temp, let hum = hum, let soilPH = soilPH {
-//                prediction = try model.prediction(
-//                    Soil_Moisture: moist,
-//                    Soil_Temperature: temp,
-//                    Humidity: hum,
-//                    Soil_pH: soilPH,
-//                    Nitrogen_Level: nitrogenLevel ?? 0.0,
-//                    Phosphorus_Level: phosphorusLevel ?? 0.0,
-//                    Potassium_Level: potassiumLevel ?? 0.0
-//                )
-//            }
-//            
-//            plantHealth = prediction.Plant_Health_Status
-//            plantHealthProbability = prediction.Plant_Health_StatusProbability
-//
-//
-//        } catch {
-//            print("Error loading model: \(error.localizedDescription)")
-//        }
-//    }
-//
-//    func predictNutrition(
-//        temp: Double? = nil,
-//        hum: Double? = nil,
-//        moist: Double? = nil
-//    ) {
-//        do {
-//            let config = MLModelConfiguration()
-//            let nitrogenModel = try NitrogenRegressor(configuration: config)
-//            let phosporusModel = try PhosphorousRegressor(configuration: config)
-//            let potassiumModel = try PotassiumRegressor(configuration: config)
-//
-//            var nitrogenPrediction: NitrogenRegressorOutput!
-//            var phosporusPrediction: PhosphorousRegressorOutput!
-//            var potassiumPrediction: PotassiumRegressorOutput!
-//
-//            if let humidity = hum, let temperature = temp, let moisture = moist
-//            {
-//                nitrogenPrediction = try nitrogenModel.prediction(
-//                    Temparature: temperature,
-//                    Humidity: humidity,
-//                    Moisture: moisture
-//                )
-//                phosporusPrediction = try phosporusModel.prediction(
-//                    Temparature: temperature,
-//                    Humidity: humidity,
-//                    Moisture: moisture
-//                )
-//                potassiumPrediction = try potassiumModel.prediction(
-//                    Temparature: temperature,
-//                    Humidity: humidity,
-//                    Moisture: moisture
-//                )
-//
-//            } else {
-//                nitrogenLevel = 0.0
-//                phosphorusLevel = 0.0
-//                potassiumLevel = 0.0
-//            }
-//
-//            nitrogenLevel = nitrogenPrediction.Nitrogen
-//            phosphorusLevel = phosporusPrediction.Phosphorous
-//            potassiumLevel = potassiumPrediction.Potassium
-//
-//        } catch {
-//            print("Error loading model: \(error.localizedDescription)")
-//        }
-//    }
-//}
+
+import CoreML
+import Foundation
+
+class AnalyzeDataViewModel: ObservableObject {
+    @Published var result: String?
+    @Published var error: Error?
+    @Published var isAnalyzing = false
+    @Published var nitrogenLevel: Double?
+    @Published var phosphorusLevel: Double?
+    @Published var potassiumLevel: Double?
+
+    @Published var plantHealth: String?
+    @Published var plantHealthProbability: [String: Double]?
+
+    func predictHealth(moist: Double? = nil, temp: Double? = nil, hum: Double? = nil, soilPH: Double? = nil) {
+        do {
+            let config = MLModelConfiguration()
+            let model = try PlantStressClassifier(configuration: config)
+
+            var prediction: PlantStressClassifierOutput!
+            
+            if let moist = moist, let temp = temp, let hum = hum, let soilPH = soilPH {
+                prediction = try model.prediction(
+                    Soil_Moisture: moist,
+                    Soil_Temperature: temp,
+                    Humidity: hum,
+                    Soil_pH: soilPH,
+                    Nitrogen_Level: nitrogenLevel ?? 0.0,
+                    Phosphorus_Level: phosphorusLevel ?? 0.0,
+                    Potassium_Level: potassiumLevel ?? 0.0
+                )
+            }
+            
+            plantHealth = prediction.Plant_Health_Status
+            plantHealthProbability = prediction.Plant_Health_StatusProbability
+
+
+        } catch {
+            print("Error loading model: \(error.localizedDescription)")
+        }
+    }
+
+    func predictNutrition(
+        temp: Double? = nil,
+        hum: Double? = nil,
+        moist: Double? = nil
+    ) {
+        do {
+            let config = MLModelConfiguration()
+            let nitrogenModel = try NitrogenRegressor(configuration: config)
+            let phosporusModel = try PhosphorousRegressor(configuration: config)
+            let potassiumModel = try PotassiumRegressor(configuration: config)
+
+            var nitrogenPrediction: NitrogenRegressorOutput!
+            var phosporusPrediction: PhosphorousRegressorOutput!
+            var potassiumPrediction: PotassiumRegressorOutput!
+
+            if let humidity = hum, let temperature = temp, let moisture = moist
+            {
+                nitrogenPrediction = try nitrogenModel.prediction(
+                    Temparature: temperature,
+                    Humidity: humidity,
+                    Moisture: moisture
+                )
+                phosporusPrediction = try phosporusModel.prediction(
+                    Temparature: temperature,
+                    Humidity: humidity,
+                    Moisture: moisture
+                )
+                potassiumPrediction = try potassiumModel.prediction(
+                    Temparature: temperature,
+                    Humidity: humidity,
+                    Moisture: moisture
+                )
+
+            } else {
+                nitrogenLevel = 0.0
+                phosphorusLevel = 0.0
+                potassiumLevel = 0.0
+            }
+
+            nitrogenLevel = nitrogenPrediction.Nitrogen
+            phosphorusLevel = phosporusPrediction.Phosphorous
+            potassiumLevel = potassiumPrediction.Potassium
+
+        } catch {
+            print("Error loading model: \(error.localizedDescription)")
+        }
+    }
+}
--- Chlorophyll/Chlorophyll/Features/Analyze/AnswerQuestion/AnswerQuestionView.swift
+++ Chlorophyll/Chlorophyll/Features/Analyze/AnswerQuestion/AnswerQuestionView.swift
@@ -14,171 +14,195 @@ struct AnswerQuestionView: View {
     @State private var navigatetoResult: Bool = false
 
     var body: some View {
-        VStack {
-            StepBar(currentStep: 3)
-            Spacer()
+        NavigationStack {
+            VStack {
+                StepBar(currentStep: 3)
+                Spacer()
 
-            HStack (spacing: 20){
+                HStack(spacing: 20) {
 
-                if let temperature = viewModel.temperature,
-                    let humidity = viewModel.humidity,
-                    let maxTemp = viewModel.maxTemp,
-                    let minTemp = viewModel.minTemp
-                {
-                    VStack {
-                        Text("\(String(format: "%.1f", temperature))°C")
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundStyle(.darkCharcoal300)
-                        Text(
-                            "min \(String(format: "%.1f", minTemp))° - max \(String(format: "%.1f", maxTemp))°"
-                        )
-                        .font(.caption)
-                        .foregroundStyle(.darkCharcoal300)
+                    if let temperature = viewModel.temperature,
+                        let humidity = viewModel.humidity,
+                        let maxTemp = viewModel.maxTemp,
+                        let minTemp = viewModel.minTemp
+                    {
+                        Button("text") {
+                            viewModel.calculateIndoorTemperature()
+                            viewModel.calculateIndoorHumidity()
+                        }
                         
-                        Text("Hum \(String(format: "%.1f", humidity))%"
-                        )
-                        .font(.title2)
-                        .foregroundStyle(.darkCharcoal300)
-                        .fontWeight(.medium)
-                    }
-                    
-                    VStack {
-                        Image(systemName: "cloud.sun.fill")
-                            .font(.system(size: 50))
-                            .foregroundStyle(.orange)
+                        Text("humIndor, \(String(format: "%.1f", viewModel.indoorHumidity ?? 0.0))%, tempIndor, \(String(format: "%.1f", viewModel.indoorTemperature ?? 0.0))°C")
                         
-                        Text("\(viewModel.locationName)")
+                        VStack {
+                            Text("\(String(format: "%.1f", temperature))°C")
+                                .font(.largeTitle)
+                                .bold()
+                                .foregroundStyle(.darkCharcoal300)
+                            Text(
+                                "min \(String(format: "%.1f", minTemp))° - max \(String(format: "%.1f", maxTemp))°"
+                            )
+                            .font(.caption)
                             .foregroundStyle(.darkCharcoal300)
-                            .bold()
+
+                            Text(
+                                "Hum \(String(format: "%.1f", humidity))%"
+                            )
+                            .font(.title2)
+                            .foregroundStyle(.darkCharcoal300)
+                            .fontWeight(.medium)
+                        }
+
+                        VStack {
+                            Image(systemName: "cloud.sun.fill")
+                                .font(.system(size: 50))
+                                .foregroundStyle(.orange)
+
+                            Text("\(viewModel.locationName)")
+                                .foregroundStyle(.darkCharcoal300)
+                                .bold()
+                        }
+
+                    } else {
+                        Text("Loading...")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    //                Text("\(viewModel.temperature)")
+                }
+                Spacer()
+                Text("Help us know your plant's spot")
+                    .font(.h3)
+                    .foregroundStyle(.darkCharcoal700)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 24)
+                    .frame(width: 240)
+                HStack(spacing: 12) {
+
+                    // Option 1
+                    Button {
+                        optionChoose = 1
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(.mughalGreen300)
+                                .cornerRadius(12)
+                                .frame(width: 150, height: 200)
+                                .clipped()
+                                .overlay {
+                                    VStack(spacing: 12) {
+                                        Image("plant-indoor")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .clipped()
+                                            .frame(width: 150)
+
+                                        Text("Indoor")
+                                            .font(.baseMedium)
+                                            .foregroundStyle(
+                                                optionChoose == 1
+                                                    ? .cultured300
+                                                    : .darkCharcoal500
+                                            )
+                                            .padding(.bottom)
+                                    }
+                                    .frame(width: 148, height: 198)
+                                    .background(optionChoose == 1 ? .clear : .white)
+                                    .cornerRadius(12)
+                                }
+
+                        }
+
                     }
 
-                    
+                    // Option 2
+                    Button {
+                        optionChoose = 2
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(.mughalGreen300)
+                                .cornerRadius(12)
+                                .frame(width: 150, height: 200)
+                                .clipped()
+                                .overlay {
+                                    VStack(spacing: 12) {
+                                        Image("plant-outdoor")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .clipped()
+                                            .frame(width: 150)
+
+                                        Text("Outdoor")
+                                            .font(.baseMedium)
+                                            .foregroundStyle(
+                                                optionChoose == 2
+                                                    ? .cultured300
+                                                    : .darkCharcoal500
+                                            )
+                                            .padding(.bottom)
+                                    }
+                                    .frame(width: 148, height: 198)
+                                    .background(optionChoose == 2 ? .clear : .white)
+                                    .cornerRadius(12)
+                                }
+
+                        }
+
+                    }
+
+                }
+                .frame(maxWidth: 280)
+
+                Spacer()
+                Spacer()
+
+                // Check Model Works or not
+                Text("Predition Output")
+
+                if let soilpH = soilpH {
+                    Text("Soil pH : \(soilpH)")
                 } else {
-                    Text("Loading...")
-                        .font(.caption)
-                        .foregroundStyle(.gray)
+                    Text("No prediction yet")
                 }
-                //                Text("\(viewModel.temperature)")
-            }
-            Spacer()
-            Text("Help us know your plant's spot")
-                .font(.h3)
-                .foregroundStyle(.darkCharcoal700)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 24)
-                .frame(width: 240)
-            HStack(spacing: 12) {
 
-                // Option 1
-                Button {
-                    optionChoose = 1
-                } label: {
-                    ZStack {
-                        Rectangle()
-                            .fill(.mughalGreen300)
-                            .cornerRadius(12)
-                            .frame(width: 150, height: 200)
-                            .clipped()
-                            .overlay {
-                                VStack(spacing: 12) {
-                                    Image("plant-indoor")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .clipped()
-                                        .frame(width: 150)
+                if let soilMoisture = viewModel.soilMoisturePrediction,
+                    let soilTemperature = viewModel.soilTempPrediction
+                {
+                    Text(
+                        "Soil Moisture :\(String(format: "%.1f", soilMoisture)) / Soil Temperature : \(String(format: "%.1f", soilTemperature))°C"
+                    )
+                } else {
+                    Text("No prediction yet")
+                }
 
-                                    Text("Indoor")
-                                        .font(.baseMedium)
-                                        .foregroundStyle(
-                                            optionChoose == 1
-                                                ? .cultured300
-                                                : .darkCharcoal500
-                                        )
-                                        .padding(.bottom)
-                                }
-                                .frame(width: 148, height: 198)
-                                .background(optionChoose == 1 ? .clear : .white)
-                                .cornerRadius(12)
-                            }
+                ActionButton(
+                    title: .submit,
+                    action: {
+                        // action for answer color and get prediction
 
+                        // test check model
+                        viewModel.getAllPredictions(option: optionChoose)
+
+                        navigatetoResult = true
                     }
-
-                }
-
-                // Option 2
-                Button {
-                    optionChoose = 2
-                } label: {
-                    ZStack {
-                        Rectangle()
-                            .fill(.mughalGreen300)
-                            .cornerRadius(12)
-                            .frame(width: 150, height: 200)
-                            .clipped()
-                            .overlay {
-                                VStack(spacing: 12) {
-                                    Image("plant-outdoor")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .clipped()
-                                        .frame(width: 150)
-
-                                    Text("Outdoor")
-                                        .font(.baseMedium)
-                                        .foregroundStyle(
-                                            optionChoose == 2
-                                                ? .cultured300
-                                                : .darkCharcoal500
-                                        )
-                                        .padding(.bottom)
-                                }
-                                .frame(width: 148, height: 198)
-                                .background(optionChoose == 2 ? .clear : .white)
-                                .cornerRadius(12)
-                            }
-
+                )
+                .navigationDestination(
+                    isPresented: $navigatetoResult,
+                    destination: {
+                        LoadingView(
+                            humidity: viewModel.humidity,
+                            temperature: viewModel.temperature,
+                            soilMoisture: viewModel.soilMoisturePrediction,
+                            soilTemperature: viewModel.soilTempPrediction,
+                            soilpH: soilpH
+                        )
                     }
-
-                }
+                )
 
             }
-            .frame(maxWidth: 280)
-
-            Spacer()
-            Spacer()
-            
-            // Check Model Works or not
-            Text("Predition Output")
-            
-            if let soilpH = soilpH {
-                Text("Soil pH : \(soilpH)")
-            } else {
-                Text("No prediction yet")
-            }
-            
-            if let soilMoisture = viewModel.soilMoisturePrediction, let soilTemperature = viewModel.soilTempPrediction {
-                Text("Soil Moisture :\(String(format: "%.1f", soilMoisture)) / Soil Temperature : \(String(format: "%.1f", soilTemperature))°C")
-            } else {
-                Text("No prediction yet")
-            }
-
-
-            ActionButton(
-                title: .submit,
-                action: {
-                    // action for answer color and get prediction
-                    
-                    // test check model
-                    viewModel.getAllPredictions(option: optionChoose)
-                    
-                    navigatetoResult = true
-                }
-            )
-//            .navigationDestination(isPresented: $navigatetoResult, destination: {LoadingView(humidity: viewModel.humidity, temperature: viewModel.temperature, soilMoisture: viewModel.soilMoisturePrediction, soilTemperature: viewModel.soilTempPrediction, soilpH: soilpH)})
-
+            .padding()
         }
-        .padding()
+
 
     }
 
